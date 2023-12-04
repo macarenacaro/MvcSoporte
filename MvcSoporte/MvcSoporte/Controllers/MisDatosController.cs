02421a5f48ea4abc19c3593f11841747e8c0259d
Macarena Caro <maccarcor@alu.edu.gva.es>
2023-12-04 10:44:08 +0100

    pag. 56 mas o menos

---
+++ MvcSoporte/MvcSoporte/Controllers/MisDatosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MvcSoporte.Data;
+using MvcSoporte.Models;
+using System.Data;
+
+namespace MvcSoporte.Controllers
+{
+    [Authorize(Roles = "Usuario")]
+    public class MisDatosController : Controller
+    {
+        private readonly MvcSoporteContexto _context;
+        public MisDatosController(MvcSoporteContexto context)
+        {
+            _context = context;
+        }
+        // GET: MisDatos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST: MisDatos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult>
+        Create([Bind("Id,Nombre,Email,Telefono,FechaNacimiento")] Empleado empleado)
+        {
+            // Asignar el Email del usuario actual
+            empleado.Email = User.Identity.Name;
+            if (ModelState.IsValid)
+            {
+                _context.Add(empleado);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(empleado);
+        }
+    }
+}
